965720e50f5dc73af8a045df09509a0988ed1a8c
Gonzalo-Leyes <gonza9gag@gmail.com>
2024-09-03 23:22:27 -0300

    page under construction...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-import Image from "next/image";
-import MotionWrapper from "./components/MotionWrapper";
+import SiteUnderConstruction from "./components/SiteUnderConstruction";
 
 export default function Home() {
   return (
-    <MotionWrapper>
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="w-full max-w-5xl flex-col items-center font-mono text-sm lg:flex">
-        <p className="top-20 mb-10 flex w-full justify-center">
-          Sitio en construcción.
-        </p>
-        <Image
-          src="/images/bonfirelogofulltext-white-square.svg"
-          alt="Vercel Logo"
-          className="white"
-          width={500}
-          height={600}
-          quality={100}
-          priority
-        />
-      </div>
-    </main>
-    </MotionWrapper>
+    <div>
+      <SiteUnderConstruction/>
+    </div>
   );
 }
