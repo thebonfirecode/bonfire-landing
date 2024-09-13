11aa1b181bd4e630e2f1de9c42319be3c70b15c3
deborolon <debora.nrolon@gmail.com>
2024-09-12 21:43:52 -0300

    feature/ generic cards for info section added

--- src/app/components/GenericCards.tsx
+++ src/app/components/GenericCards.tsx
@@ -1,12 +1,40 @@
+import Image from "next/image";
+
 interface Props {
-    description: string,
-    style: string
+  imgPath: string;
+  imgWidth: number;
+  imgHeight: number;
+  title: string;
+  description: string;
+  link?: string;
 }
 
-export default function GenericCards({ description, style }: Props) {
+export default function GenericCards({
+  imgPath,
+  imgWidth,
+  imgHeight,
+  title,
+  description,
+  link
+}: Props) {
   return (
-    <div>
-        <p className={style}>{description}</p>
+    <div className="max-w-sm p-6 bg-[#eff0ef] rounded-lg shadow mx-3 hover:bg-main-orange text-main-black hover:text-white">
+      <Image
+        src={imgPath}
+        alt="logo"
+        width={imgWidth}
+        height={imgHeight}
+        priority
+      />
+      <p className="mb-2 text-2xl font-semibold tracking-tight">
+        {title}
+      </p>
+      <p className="mb-3 font-normal">
+        {description}
+      </p>
+      <p className="inline-flex font-bold items-center">
+        {link}
+      </p>
     </div>
   );
 }
