82830dfb745cb015dea93f40f90050a69964624d
deborolon <debora.nrolon@gmail.com>
2024-09-19 19:50:12 -0300

    feature/ new banner, update cards & services

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,29 @@
+import DinamicText from "./DinamicText";
+
+interface BannerProps {
+  title: string;
+  description: string;
+  imgSrc: string;
+}
+
+const Banner: React.FC<BannerProps> = ({ title, description, imgSrc }) => {
+  return (
+    <div className="flex flex-col md:flex-row items-center px-8 py-28 bg-black">
+      <div className="max-w-auto text-right">
+        <DinamicText
+          description={title}
+          style="text-main-orange font-sans font-black text-6xl mb-4 tracking-widest leading-loose"
+        />
+        <DinamicText
+          description={description}
+          style="text-main-orange font-sm text-lg mb-2"
+        />
+      </div>
+      <div className="my-2">
+        <img src={imgSrc} alt="Img" className="w-auto h-auto" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Video from "./components/Video";
-import DinamicText from "./components/DinamicText"
+import DinamicText from "./components/DinamicText";
 import GenericCards from "./components/GenericCards";
+import cardsInfo from "../app/data/cardsInfo.json";
+import SvgComponents from "./components/SvgComponents";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
@@ -8,34 +11,49 @@ export default function Home() {
       <div className="static">
         <Video videoSource={"assets/88968-608446385_medium.mp4"}></Video>
         <div className="absolute top-1/4 left-14">
-          <DinamicText description="TECNOLOGÍA A TU MEDIDA" style="font-extrabold text-5xl w-52 mb-6 tracking-widest leading-loose"/>
-          <DinamicText description="Impulsá tu negocio con soluciones IT eficientes e integradas para transformar tu empresa simplificando operaciones y mejorando la productividad con un enfoque práctico, resultados medibles y soporte continuo de la mano de nuestros expertos" style="w-2/4 font-base tracking-widest leading-loose"/>
+          <DinamicText
+            description="TECNOLOGÍA A TU MEDIDA"
+            style="font-extrabold text-5xl w-52 mb-6 tracking-widest leading-loose"
+          />
+          <DinamicText
+            description="Impulsá tu negocio con soluciones IT eficientes e integradas para transformar tu empresa simplificando operaciones y mejorando la productividad con un enfoque práctico, resultados medibles y soporte continuo de la mano de nuestros expertos"
+            style="w-2/4 font-base tracking-widest leading-loose"
+          />
         </div>
-        <div className="flex justify-center my-10">
-        <GenericCards
-            imgPath="/images/safe-lock.svg"
-            imgWidth={60}
-            imgHeight={60}
-            title="Consultoría IT"
-            description="Te asesoramos en cada paso para implementar soluciones robustas y escalables"
-            link="Ver más →"
+        <div className="mx-20 mt-12">
+          <DinamicText
+            description="NUESTROS SERVICIOS"
+            style="text-main-orange font-extrabold text-lg mb-2 tracking-widest leading-loose"
           />
-          <GenericCards
-            imgPath="/images/service.svg"
-            imgWidth={60}
-            imgHeight={60}
-            title="Consultoría IT"
-            description="Te asesoramos en cada paso para implementar soluciones robustas y escalables"
-            link="Ver más →"
+          <DinamicText
+            description="Qué ofrecemos?"
+            style="text-main-black font-extrabold text-4xl mb-2 tracking-widest leading-loose"
           />
-          <GenericCards
-            imgPath="/images/rocket.svg"
-            imgWidth={60}
-            imgHeight={60}
-            title="Consultoría IT"
-            description="Te asesoramos en cada paso para implementar soluciones robustas y escalables"
-            link="Ver más →"
-          />
+          <div className="max-w-[600px]">
+            <DinamicText
+              description="Impulsa tu negocio con nuestro enfoque integral: desde asesoramiento estratégico y herramientas digitales innovadoras hasta desarrollo personalizado y soporte técnico de alta calidad"
+              style="text-main-black font-base text-md mb-6"
+            />
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
+            {cardsInfo.map((card, index) => {
+              return (
+                <GenericCards
+                  key={index}
+                  SvgIcon={SvgComponents[card.imgPath]}
+                  title={card.title}
+                  description={card.description}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="w-full h-auto mt-12 mb-44">
+            <Banner
+              imgSrc="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzFzeW5jcXBjcXE5ZmJodjZibWZmeGw3dWJxeWZ5OXgzYjNsNGtmaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/joVdaoY8dJ70Ai8pSD/giphy.gif"
+              title="Go Digital"
+              description="En nuestra hoguera tecnológica, te acompañamos con estrategias robustas y creativas para que siempre sientas la tranquilidad de estar en las mejores manos"
+            />
         </div>
       </div>
     </div>
